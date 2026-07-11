653a326b2ddd5dd88bbb9d7a560dc20e98df43db
D@nrol <danrol@uol.com.br>
2026-07-11 20:29:50 -0300

    feat: keep screen awake using wakelock_plus

--- Desktop/acls_guide_backup/acls_guide/lib/main.dart
+++ Desktop/acls_guide_backup/acls_guide/lib/main.dart
@@ -2,7 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'app.dart';
 
+import 'package:wakelock_plus/wakelock_plus.dart';
+
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  WakelockPlus.enable();
+  
   runApp(
     const ProviderScope(
       child: AclsApp(),
